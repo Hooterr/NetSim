1741ae88bdceba3a467ca91c3836c1f175ef415a
Maksymilian Lach <maks.lach99@gmail.com>
2020-01-03 14:20:09 +0100

    Empty files for next stage

---
+++ src/include/helpers.hpp
@@ -0,0 +1,8 @@
+//
+// Created by root on 1/3/2020.
+//
+
+#ifndef SRC_HELPERS_HPP
+#define SRC_HELPERS_HPP
+
+#endif //SRC_HELPERS_HPP
---
+++ src/include/nodes.hpp
@@ -0,0 +1,8 @@
+//
+// Created by root on 1/3/2020.
+//
+
+#ifndef SRC_NODES_HPP
+#define SRC_NODES_HPP
+
+#endif //SRC_NODES_HPP
