5297e65b90fa733380f4d1188d211a701f54d2a1
Maksymilian Lach <maks.lach99@gmail.com>
2020-01-03 16:01:38 +0100

    Nodes more work

--- src/include/nodes.hpp
+++ src/include/nodes.hpp
@@ -15,17 +15,24 @@ class IPackageReceiver
 {
 public:
     virtual void receive_package(Package &&p) = 0;
-    virtual ElementID get_id() = 0;
+    virtual ElementID get_id() const = 0;
 };
 
 class Storehouse : public IPackageReceiver{
 public:
-    Storehouse(ElementID id, std::unique_ptr<IPackageStockpile> d);
+    Storehouse(ElementID id, std::unique_ptr<IPackageStockpile> d) : id_(id){
+        sp_ = std::move(d);
+    }
+
+private:
+    ElementID id_;
+    std::unique_ptr<IPackageStockpile> sp_;
 };
 
 class ReceiverPreferences {
 public:
     ReceiverPreferences(ProbabilityGeneretor pg);
+    ReceiverPreferences() {} // DELETE THAT LATER
     void add_receiver(IPackageReceiver* receiver);
     void remove_receiver(IPackageReceiver* receiver);
     IPackageReceiver* choose_receiver();
@@ -34,29 +41,44 @@ public:
 class PackageSender {
 public:
     ReceiverPreferences receiver_preferences;
-
     void send_package();
     std::optional<Package> get_sending_buffer();
-
 protected:
     void push_package(Package && package);
+
+private:
+    std::optional<Package> buffer_;
 };
 
 class Ramp : public PackageSender{
 public:
-    Ramp(ElementID id, TimeOffset di);
+    Ramp(ElementID id, TimeOffset di) : id_(id), di_(di) {}
     void deliver_goods(Time t);
-    TimeOffset get_delivery_interval() const;
-    ElementID get_id() const;
+    TimeOffset get_delivery_interval() const { return di_; }
+    ElementID get_id() const { return id_; }
 
+private:
+    TimeOffset di_;
+    ElementID id_;
+    Time start_time;
 };
 
 class Worker : public IPackageReceiver, PackageSender {
 public:
-    Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> pq);
-    void do_work(Time t);
-    TimeOffset get_processing_duration() const;
-    Time get_package_processing_start_time() const;
+    Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> pq) : id_(id), pd_(pd) {
+        pq_ = std::move(pq);
+    }
+    void do_work(Time time);
+    TimeOffset get_processing_duration() const { return pd_; }
+    Time get_package_processing_start_time() const { return start_time; }
+    void receive_package(Package &&p);
+    ElementID get_id() const { return id_;}
+
+private:
+    ElementID id_;
+    TimeOffset pd_;
+    Time start_time;
+    std::unique_ptr<IPackageQueue> pq_;
 };
 
 #endif //SRC_NODES_HPP
--- src/nodes.cpp
+++ src/nodes.cpp
@@ -2,3 +2,50 @@
 // Created by root on 1/3/2020.
 //
 
+#include <nodes.hpp>
+
+void Worker::do_work(Time time) {
+    if (get_sending_buffer())
+    {
+        if (abs(time - start_time) >= pd_)
+        {
+            // We're done with this package
+            push_package(std::move(get_sending_buffer().value()));
+        }
+    }
+    else
+    {
+        if (pq_->empty() == false) {
+            Package &package = pq_->pop();
+            push_package(std::move(package));
+            start_time = time;
+        }
+    }
+}
+
+void Worker::receive_package(Package &&p) {
+    pq_->push(std::move(p));
+}
+
+void Ramp::deliver_goods(Time t) {
+    if(abs(start_time - t) >= di_)
+    {
+        push_package(Package());
+        start_time = t;
+    }
+}
+
+void PackageSender::send_package() {
+
+}
+
+std::optional<Package> PackageSender::get_sending_buffer() {
+    return std::move(buffer_);
+}
+
+void PackageSender::push_package(Package &&package) {
+    buffer_.emplace(std::move(package));
+}
+
+
+
